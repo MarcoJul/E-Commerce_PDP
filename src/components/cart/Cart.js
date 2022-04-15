f25a35fcf8e00503cd7ce659fba1a2078f561740
marco <marco.giuliotti@gmail.com>
2022-04-15 20:15:47 +0200

    feat: added dynamic content to cart

--- src/components/cart/Cart.js
+++ src/components/cart/Cart.js
@@ -1,13 +1,33 @@
 import classes from "./Cart.module.css";
 
+import Button from "../UI/Button";
+
+import { ReactComponent as TrashIcon } from "../../images/icon-delete.svg";
+
 const Cart = () => {
   return (
     <div className={classes.cart}>
       <div className={classes.titleCart}>
         <h2>Cart</h2>
       </div>
-      <div className={classes.cartContent}>
+      {/* <div className={classes.cartContent}>
         <p>Your Cart is Empty.</p>
+      </div> */}
+      <div className={classes.productArea}>
+        <img className={classes.productImage} src={`/images/image-product-${1}-thumbnail.jpg`} alt="product" />
+        <div>
+          <p className={classes.productTitle}>Fall Limited Edition Sneakers</p>
+          <div className={classes.productPrice}>
+            <p>
+              $125.00 <span>{`x ${3}`}</span>
+            </p>
+            <p className={classes.totalPrice}>$375.00</p>
+          </div>
+        </div>
+        <TrashIcon />
+      </div>
+      <div className={classes.button}>
+        <Button message="Checkout" />
       </div>
     </div>
   );
