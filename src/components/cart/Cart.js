b352beb924be63c410ea428ec17f8e86e0d126d1
marco <marco.giuliotti@gmail.com>
2022-04-15 23:49:23 +0200

    style: cart style completed

--- src/components/cart/Cart.js
+++ src/components/cart/Cart.js
@@ -3,32 +3,42 @@ import classes from "./Cart.module.css";
 import Button from "../UI/Button";
 
 import { ReactComponent as TrashIcon } from "../../images/icon-delete.svg";
+import { Fragment } from "react";
 
 const Cart = () => {
+  let content = false;
+
   return (
     <div className={classes.cart}>
       <div className={classes.titleCart}>
         <h2>Cart</h2>
       </div>
-      {/* <div className={classes.cartContent}>
-        <p>Your Cart is Empty.</p>
-      </div> */}
-      <div className={classes.productArea}>
-        <img className={classes.productImage} src={`/images/image-product-${1}-thumbnail.jpg`} alt="product" />
-        <div>
-          <p className={classes.productTitle}>Fall Limited Edition Sneakers</p>
-          <div className={classes.productPrice}>
-            <p>
-              $125.00 <span>{`x ${3}`}</span>
-            </p>
-            <p className={classes.totalPrice}>$375.00</p>
-          </div>
+      {content ? (
+        <div className={classes.cartContent}>
+          <p>Your Cart is Empty.</p>
         </div>
-        <TrashIcon />
-      </div>
-      <div className={classes.button}>
-        <Button message="Checkout" />
-      </div>
+      ) : (
+        <Fragment>
+          <div className={classes.productArea}>
+            <img className={classes.productImage} src={`/images/image-product-${1}-thumbnail.jpg`} alt="product" />
+            <div>
+              <p className={classes.productTitle}>Fall Limited Edition Sneakers</p>
+              <div className={classes.productPrice}>
+                <p>
+                  $125.00 <span>{`x ${3}`}</span>
+                </p>
+                <p className={classes.totalPrice}>$375.00</p>
+              </div>
+            </div>
+            <div className={classes.trashBtn}>
+              <TrashIcon />
+            </div>
+          </div>
+          <div className={classes.button}>
+            <Button message="Checkout" />
+          </div>
+        </Fragment>
+      )}
     </div>
   );
 };
